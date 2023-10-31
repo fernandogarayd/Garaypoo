c43d5f232ba7ef43f0dee054a5a15492ec75a43f
GARAY <fernando.garay@utm.edu.ec>
2023-10-30 19:17:20 -0500

    Commit 2: Método para configuración inicial

--- src/main/java/com/mycompany/garaypoo/DeberCAI.java
+++ src/main/java/com/mycompany/garaypoo/DeberCAI.java
@@ -13,5 +13,18 @@ public class DeberCAI {
         aciertos = 0;
         intentos = 0;
     }
+        public void configuracionInicial() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Elige el nivel de dificultad (1, 2, 3): ");
+        nivelDificultad = scanner.nextInt();
+        System.out.print("Elige el tipo de problema aritmético "
+        + "(1: Suma, "
+        + "2: Resta, "
+        + "3: Multiplicación, "
+        + "4: División, "
+        + "5: Aleatorio): ");
+        tipoProblema = scanner.nextInt();
+    }
+
 }
 
