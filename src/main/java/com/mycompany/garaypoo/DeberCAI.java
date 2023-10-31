c165dd33f3d891e5666b5f391cf059ee2c4138bc
GARAY <fernando.garay@utm.edu.ec>
2023-10-30 19:13:58 -0500

    Commit 1: Creación de la clase y variables de instancia

---
+++ src/main/java/com/mycompany/garaypoo/DeberCAI.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class DeberCAI {
+    private int aciertos;
+    private int intentos;
+    private int nivelDificultad;
+    private int tipoProblema;
+    private Random random;
+
+    public DeberCAI() {
+        random = new Random();
+        aciertos = 0;
+        intentos = 0;
+    }
+}
+
